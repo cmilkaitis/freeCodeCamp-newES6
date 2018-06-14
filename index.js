a2a93cc4316406e3b93f2d4d76c5f2803c815f13
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-13 22:13:55 -0400

    4 es6 lessons

--- index.js
+++ index.js
@@ -16,6 +16,51 @@ const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
 (function() {
   "use strict";
-  arr2 = [...arr1]; // change this line
+  arr2 = [...arr1];
 })();
 console.log(arr2);
+
+// Template Literals
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  const resultDisplayArray = arr.map(item => {
+    return `<li class="text-warning">${item}</li>`;
+  })
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ <li class="text-warning">no-var</li>,
+ *   <li class="text-warning">var-on-top</li>, 
+ *   <li class="text-warning">linebreak</li> ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+
+
+// Write Concise Object Literal Declarations Using Simple Fields
+const createPerson = (name, age, gender) => {
+  "use strict";
+  return ({name, age, gender});
+};
+
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
+
+// Concise Declarative Functions
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
